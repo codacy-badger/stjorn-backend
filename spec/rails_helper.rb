209ed17b6c8f6b8cc252d642995900714ad9ed87
kkm <kubasurdej@gmail.com>
2019-04-11 15:03:13 +0200

    Add RequestSpecHelper Custom module containing json method whitch is parsed request body

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,6 +25,7 @@ end
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.include RequestSpecHelper
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
