441bb6cc268f67aa4c947f8eb2fb131e2fc2ce9d
kkm <kubasurdej@gmail.com>
2019-04-15 23:53:55 +0200

    Support files for graphql testing

---
+++ spec/support/graphql/mutations_helper.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+module GraphQL
+  module MutationsHelper
+    def create_post_mutation
+      %(
+				mutation CreatePost(
+					$title: String!,
+					$body: String!,
+				) {
+					createPost(input: {
+						title:$title,
+						body:$body,
+					}) {
+						post {
+							id
+							title
+							body
+						}
+						errors
+					}
+				}
+			)
+    end
+
+    def create_user_mutation
+      %(
+				mutation CreateUser(
+					$username: String!,
+					$email: String!,
+					$password: String!,
+				) {
+					createUser(input: {
+						username:$username,
+						email:$email,
+						password:$password,
+					}) {
+						user {
+							id
+							username
+							email
+						}
+						token,
+						errors,
+					}
+				}
+			)
+    end
+
+    def login_mutation
+      %(
+				mutation Login(
+					$email: String!,
+					$password: String!,
+				) {
+					login(input: {
+						email:$email,
+						password:$password,
+					}) {
+						token,
+						errors,
+					}
+				}
+			)
+    end
+  end
+end
